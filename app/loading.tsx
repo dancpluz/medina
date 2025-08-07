b9cc5bd5d609c04b4859d00ae6a4ddf313c3c3f6
dancpluz <dan08jan@gmail.com>
2025-08-06 22:11:39 -0300

    feat: add better loading state

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-      <p>Carregando…</p>
+      <div className="cube-loader">
+        <div className="cube-top"></div>
+        <div className="cube-wrapper">
+          <span className="cube-span [--i:0]" />
+          <span className="cube-span [--i:1]" />
+          <span className="cube-span [--i:2]" />
+          <span className="cube-span [--i:3]" />
+        </div>
+      </div>
     </div>
   )
 }
